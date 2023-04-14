cecb05d5e9b5fe048373024d0c1666f184d7a49a
Jan Gruenwaldt <jgruen02@tufts.edu>
2023-04-14 13:46:07 +0200

    fix config crash

--- utils/config.py
+++ utils/config.py
@@ -1,30 +1,34 @@
 import json
+import os
 
 from destinations import ROOT_DIR
 
 CONFIG_PATH = f'{ROOT_DIR}/config.json'
+CONFIG_DATA = {}
 
-with open(CONFIG_PATH, 'r') as f:
-    CONFIG_DATA = json.load(f)
-    print(f'Using config: {CONFIG_DATA}')
+# when running tests we do not want to fetch the config as all is mocked
+if os.path.isfile(CONFIG_PATH):
+    with open(CONFIG_PATH, 'r') as f:
+        CONFIG_DATA = json.load(f)
+        print(f'Using config: {CONFIG_DATA}')
 
 
 class Config:
     @staticmethod
     def get_pat():
-        return CONFIG_DATA['personal_access_token']
+        return CONFIG_DATA.get('personal_access_token', None)
 
     @staticmethod
     def get_db_user():
-        return CONFIG_DATA['db_user']
+        return CONFIG_DATA.get('db_user', None)
 
     @staticmethod
     def get_db_pw():
-        return CONFIG_DATA['db_pw']
+        return CONFIG_DATA.get('db_pw', None)
 
     @staticmethod
     def get_db_address():
-        return CONFIG_DATA['db_address']
+        return CONFIG_DATA.get('db_address', None)
 
     @staticmethod
     def get_github_request_header():
