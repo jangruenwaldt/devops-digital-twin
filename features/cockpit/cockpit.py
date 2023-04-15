7fab05bb95dd42a3714912aeb61a9ed7c6a2ab5a
Jan Gruenwaldt <jgruen02@tufts.edu>
2023-04-15 14:23:43 +0200

    feat: some more interesting logging info

--- features/cockpit/cockpit.py
+++ features/cockpit/cockpit.py
@@ -18,3 +18,34 @@ class Cockpit:
 
         GitTwin.construct_from_github_url(repo_url, branch_name=release_branch_name, debug_options=debug_options)
         ReleasesTwin.construct(repo_url, debug_options=debug_options)
+        Cockpit.print_usage_info()
+
+    @staticmethod
+    def print_usage_info():
+        print(""""
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@    DONE CONSTRUCTING TWIN    @@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        
+Time to explore! Visit the graph at https://www.yworks.com/neo4j-explorer/
+You might be interested in some queries:
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ View everything (not recommended for > 1000 nodes): @
+@                                                     @
+@                 match (n) return n                  @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ View releases:                                      @
+@                                                     @
+@              match (n:Release) return n             @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ View relationships between releases and commits:    @
+@                                                     @
+@ MATCH (n)-[:LATEST_INCLUDED_COMMIT]->(l)            @
+@ RETURN n, l                                         @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+""")
