c9827d2897661002b8955d02119b9b5bc4442712
Jan Gruenwaldt <jgruen02@tufts.edu>
2023-04-19 21:23:57 +0200

    feat: ProjectManagementTwin._add_issue_labels

--- features/twins/project_management_twin.py
+++ features/twins/project_management_twin.py
@@ -1,11 +1,38 @@
-from py2neo import Node
+from py2neo import Node, Relationship
 
 from features.github.github import GitHub
 from utils.graph.graph_nodes import GraphNodes
+from utils.graph.graph_relationships import GraphRelationships
 from utils.neo4j import Neo4j
 
 
 class ProjectManagementTwin:
+    @staticmethod
+    def _add_issue_labels(enable_logs, issue, issue_node):
+        graph = Neo4j.get_graph()
+
+        for label in issue['labels']:
+            existing_label = graph.nodes.match(GraphNodes.ISSUE_LABEL, id=label['id']).first()
+            if existing_label is None:
+                node_to_add = Node(GraphNodes.ISSUE_LABEL,
+                                   id=label['id'],
+                                   url=label['url'],
+                                   name=label['name'],
+                                   color=label['color'],
+                                   description=label['description'])
+                graph.create(node_to_add)
+                if enable_logs:
+                    print(f'Added issue label {label["description"]}')
+            else:
+                if enable_logs:
+                    print(f'Issue label {label["description"]} already found')
+
+            issue_label_node = graph.nodes.match(GraphNodes.ISSUE_LABEL, id=label['id']).first()
+            has_label_relationship = Relationship(issue_node, GraphRelationships.HAS_LABEL, issue_label_node)
+            graph.create(has_label_relationship)
+            if enable_logs:
+                print(f'Added relationship from {issue_node} to {issue_label_node}')
+
     @staticmethod
     def construct(github_url, enable_cache=True, debug_options=None):
         if debug_options is None:
@@ -40,3 +67,5 @@ class ProjectManagementTwin:
             graph.create(issue_node)
             if enable_logs:
                 print(f'Added issue {issue_node}')
+
+            ProjectManagementTwin._add_issue_labels(enable_logs, issue, issue_node)
