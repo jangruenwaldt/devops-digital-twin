62b1194f03a0274deab8c9c8db34372a9070fefb
Jan Gruenwaldt <jgruen02@tufts.edu>
2023-05-01 13:28:04 +0200

    refactor: move to github -> export_commit_data_as_json

--- destinations.py
+++ destinations.py
@@ -1,5 +1,5 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-TWIN_DATA_DIR = f'{ROOT_DIR}/.local-twin-data'
+LOCAL_DATA_DIR = f'{ROOT_DIR}/.local-data'
 TWIN_DATA_EXPORT_DIR = f'{ROOT_DIR}/TWIN_DATA'
--- features/github/github.py
+++ features/github/github.py
@@ -1,11 +1,63 @@
+import json
+import os
+
+from git import Repo
+
+from destinations import LOCAL_DATA_DIR, TWIN_DATA_EXPORT_DIR
 from utils.cache import Cache
 from utils.cached_request import CachedRequest
 from utils.config import Config
 
 
 class GitHub:
-    def __init__(self, repo_url):
+    def __init__(self, repo_url, branch='main'):
         self.repo_url = repo_url
+        self.branch = branch
+
+    @staticmethod
+    def setup():
+        if not os.path.exists(LOCAL_DATA_DIR):
+            os.makedirs(LOCAL_DATA_DIR)
+
+    def export_commit_data_as_json(self, debug_options=None):
+        self.setup()
+        if not os.path.exists(TWIN_DATA_EXPORT_DIR):
+            os.makedirs(TWIN_DATA_EXPORT_DIR)
+
+        if debug_options is None:
+            debug_options = {}
+        enable_logs = 'enable_logs' in debug_options and debug_options['enable_logs']
+
+        repo_owner_slash_name = self.repo_url.split("https://github.com/")[1]
+        repo_dir = f'{LOCAL_DATA_DIR}/{repo_owner_slash_name}'
+
+        if not os.path.exists(repo_dir):
+            if enable_logs:
+                print('repo did not exist locally yet, cloning now')
+            Repo.clone_from(self.repo_url, repo_dir)
+        elif enable_logs:
+            print('found repo locally')
+
+        repo = Repo(repo_dir)
+        repo.git.checkout(self.branch)
+        repo.remotes.origin.pull()
+
+        commits = []
+        for commit in repo.iter_commits(self.branch, reverse=True):
+            commit_data = {
+                'message': commit.message,
+                'hash': commit.hexsha,
+                'author': commit.author.email,
+                'date': commit.committed_datetime.replace(microsecond=0).isoformat(),
+                'branch': self.branch,
+                'url': (self.repo_url + f'/commit/{commit.hexsha}'),
+                'parents': list(map(lambda c: c.hexsha, commit.parents))
+            }
+            commits.append(commit_data)
+        repo.close()
+
+        with open(os.path.join(TWIN_DATA_EXPORT_DIR, 'commits.json'), 'w') as output_file:
+            json.dump(commits, output_file)
 
     def fetch_issues(self):
         owner, repo_name = self.get_owner_and_repo_name()
