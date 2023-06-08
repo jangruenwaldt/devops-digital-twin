41eee1a54d76eabe3a70d8d4bb2ed78bf8b8a8be
Jan Gruenwaldt <jgruen02@tufts.edu>
2023-06-08 11:52:55 +0200

    fix script

--- create_twin.py
+++ create_twin.py
@@ -3,16 +3,18 @@ import argparse
 from features.twins.twin_builder import TwinBuilder
 
 
-def main(repo_url, wipe_db, enable_logs):
-    TwinBuilder.construct_from_github_data_repo(repo_url=repo_url, debug_options={'enable_logs': enable_logs},
+def main(repo_url, twin_name, wipe_db, enable_logs):
+    TwinBuilder.construct_from_github_data_repo(repo_url=repo_url, twin_name=twin_name,
+                                                debug_options={'enable_logs': enable_logs},
                                                 wipe_db=wipe_db)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--repo_url", type=str, required=True, help="Twin data repository URL")
+    parser.add_argument("--twin_name", type=str, required=True, help="Twin name")
     parser.add_argument("--wipe_db", type=bool, default=True, help="Wipe DB")
     parser.add_argument("--enable_logs", type=bool, default=True, help="Enable Logs")
 
     args = parser.parse_args()
-    main(args.repo_url, args.wipe_db, args.enable_logs)
+    main(args.repo_url, args.twin_name, args.wipe_db, args.enable_logs)
