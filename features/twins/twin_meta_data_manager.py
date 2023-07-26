7be3659636ae2f3acbb06140316c14b807c6c675
Jan Grünwaldt <jangruenwaldt@gmail.com>
2023-07-26 12:42:44 +0200

    feat: store data sources in MetaData so we can check if it changed

--- features/twins/twin_meta_data_manager.py
+++ features/twins/twin_meta_data_manager.py
@@ -40,8 +40,13 @@ class TwinMetaDataManager:
         query = f'''
                 MERGE (m:TwinMetaData)
                 SET
-                m.project_management_incident_categories = {Config.get_project_management_incident_categories()},
-                m.deployment_automation_name = {deployment_automation_name_wrapped}
+                    m.project_management_incident_categories = {Config.get_project_management_incident_categories()},
+                    m.deployment_automation_name = {deployment_automation_name_wrapped},
+                    m.commit_data_source = {Config.get_commit_data_source()},
+                    m.deployment_data_source = {Config.get_deployment_data_source()},
+                    m.project_management_data_source = {Config.get_project_management_data_source()},
+                    m.automations_data_source = {Config.get_automations_data_source()},
+                    m.automations_history_data_source = {Config.get_automations_history_data_source()}
         '''
         result = Neo4j.run_query(query)
         print(result)
